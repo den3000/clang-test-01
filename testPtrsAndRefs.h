94daf16edb7984199b56c42a7db93963bf1d7d99
Denis Suprun <suprundenis@gmail.com>
2023-08-30 13:39:54 +0300

    + Improving logs

--- testPtrsAndRefs.h
+++ testPtrsAndRefs.h
@@ -57,13 +57,21 @@ void constSyntax() {
 
 }
 
-void fooPtrsSizesAndAdrs2(Model * pModel) {
-    cout << endl;
+void fooPtrsSizesAndAdrs2(Model model) {
     cout << "fooPtrsSizesAndAdrs2" << endl;
 
+    cout << "value of model: " << model << endl;
+    cout << "size of model: " << sizeof(model) << endl;
+    cout << "addr of model: " << &model << endl;
+}
+
+void fooPtrsSizesAndAdrs3(Model * pModel) {
+    cout << "fooPtrsSizesAndAdrs3" << endl;
+
+    cout << "value of pModel: " << pModel << endl;
+    cout << "dereferenced value of pModel: " << *pModel << endl;
     cout << "size of pModel: " << sizeof(pModel) << endl;
-    cout << "pModel value: " << pModel << endl;
-    cout << "addr of pModel: " << &(pModel) << endl;
+    cout << "addr of pModel: " << &pModel << endl;
 }
 
 void fooPtrsSizesAndAdrs() {
@@ -74,47 +82,69 @@ void fooPtrsSizesAndAdrs() {
     cout << endl;
     cout << "==== int ====" << endl;
     int x = 10;
+    cout << "value of x: " << x << endl;
     cout << "size of x: " << sizeof(x) << endl;
     cout << "addr of x: " << &x << endl;
 
     int * px = &x;
+    cout << "value of px: " << px << endl;
+    cout << "de-refed value of px: " << *px << endl;
     cout << "size of px: " << sizeof(px) << endl;
-    cout << "px value: " << px << endl;
     cout << "addr of px: " << &px << endl;
 
     // ==== string ====
     cout << endl;
     cout << "==== string ====" << endl;
     string str = "Some really really long text";
+    cout << "value of str: " << str << endl;
     cout << "size of str: " << sizeof(str) << endl;
     cout << "addr of str: " << &str << endl;
 
     string * pStr = &str;
+    cout << "value of pStr: " << pStr << endl;
+    cout << "de-refed value of pStr: " << *pStr << endl;
     cout << "size of pStr: " << sizeof(pStr) << endl;
-    cout << "pStr value: " << pStr << endl;
     cout << "addr of pStr: " << &pStr << endl;
 
     // ==== model ====
     cout << endl;
     cout << "==== model ====" << endl;
     Model model("some model");
+    cout << "value of model: " << model << endl;
     cout << "size of model: " << sizeof(model) << endl;
     cout << "addr of model: " << &model << endl;
 
     Model * pModel = &model;
+    cout << "value of pModel: " << pModel << endl;
+    cout << "de-refed value of pModel: " << *pModel << endl;
     cout << "size of pModel: " << sizeof(pModel) << endl;
-    cout << "pModel value: " << pModel << endl;
     cout << "addr of pModel: " << &pModel << endl;
 
-    fooPtrsSizesAndAdrs2(pModel);
+    cout << endl;
+    fooPtrsSizesAndAdrs2(model);
+    cout << endl;
+    fooPtrsSizesAndAdrs2(*pModel);
+
+    cout << endl;
+    fooPtrsSizesAndAdrs3(&model);
+    cout << endl;
+    fooPtrsSizesAndAdrs3(pModel);
     
     cout << endl;
 }
 
-void fooRefsSizesAndAdrs2(Model & rModel) {
-    cout << endl;
+void fooRefsSizesAndAdrs2(Model model) {
     cout << "fooRefsSizesAndAdrs2" << endl;
 
+    cout << "value of model: " << model << endl;
+    cout << "size of model: " << sizeof(model) << endl;
+    cout << "addr of model: " << &model << endl;
+}
+
+void fooRefsSizesAndAdrs3(Model & rModel) {
+    cout << "fooRefsSizesAndAdrs3" << endl;
+
+    cout << "value of rModel: " << rModel << endl;
     cout << "size of rModel: " << sizeof(rModel) << endl;
     cout << "addr of rModel: " << &rModel << endl;
 }
@@ -127,39 +157,50 @@ void fooRefsSizesAndAdrs() {
     cout << endl;
     cout << "==== int ====" << endl;
     int x = 10;
+    cout << "value of x: " << x << endl;
     cout << "size of x: " << sizeof(x) << endl;
     cout << "addr of x: " << &x << endl;
 
     int & rx = x;
+    cout << "value of rx: " << rx << endl;
     cout << "size of rx: " << sizeof(rx) << endl;
-    cout << "rx value: " << rx << endl;
     cout << "addr of rx: " << &rx << endl;
 
     // ==== string ====
     cout << endl;
     cout << "==== string ====" << endl;
     string str = "Some really really long text";
+    cout << "value of str: " << str << endl;
     cout << "size of str: " << sizeof(str) << endl;
     cout << "addr of str: " << &str << endl;
 
     string & rStr = str;
+    cout << "value of rStr: " << rStr << endl;
     cout << "size of rStr: " << sizeof(rStr) << endl;
-    cout << "rStr value: " << rStr << endl;
     cout << "addr of rStr: " << &rStr << endl;
 
     // ==== model ====
     cout << endl;
     cout << "==== model ====" << endl;
     Model model("some model");
+    cout << "value of model: " << model << endl;
     cout << "size of model: " << sizeof(model) << endl;
     cout << "addr of model: " << &model << endl;
 
     Model & rModel = model;
+    cout << "value of rModel: " << rModel << endl;
     cout << "size of rModel: " << sizeof(rModel) << endl;
     cout << "addr of rModel: " << &rModel << endl;
 
+    cout << endl;
     fooRefsSizesAndAdrs2(model);
+    cout << endl;
     fooRefsSizesAndAdrs2(rModel);
+
+    cout << endl;
+    fooRefsSizesAndAdrs3(model);
+    cout << endl;
+    fooRefsSizesAndAdrs3(rModel);
     
     cout << endl;
 }
