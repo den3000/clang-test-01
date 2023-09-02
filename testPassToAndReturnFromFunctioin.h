503b0b3010385d11c9a9c5094e3671a269088d65
Denis Suprun <suprundenis@gmail.com>
2023-09-02 23:06:57 +0300

    + testPassToFunctioinByVal example

--- testPassToAndReturnFromFunctioin.h
+++ testPassToAndReturnFromFunctioin.h
@@ -8,14 +8,33 @@ void testPassToFunctioinByPtr();
 void testPassToFunctioinByRef();
 
 void testReturnFromFunctioinByVal(); 
-void testReturnFromFunctioinByRef(); 
 void testReturnFromFunctioinByPtr(); 
+void testReturnFromFunctioinByRef(); 
 
 void testPassToAndReturnFromFunctioin() {
+    testPassToFunctioinByVal();
+}
 
+void fooPassToFunctioinByVal(Model model) {
+    cout << "addr of model: " << &model << endl;
 }
 
 void testPassToFunctioinByVal() {
+    Model model("tag0");
+    Model * pModel = &model;
+    Model & rModel = model;
+
+    cout << "fooPassToFunctioinByVal for val" << endl;
+    fooPassToFunctioinByVal(model);
+    cout << "fooPassToFunctioinByVal from ptr" << endl;
+    fooPassToFunctioinByVal(*pModel);
+    cout << "fooPassToFunctioinByVal from ref" << endl;
+    fooPassToFunctioinByVal(rModel);
+    cout << "fooPassToFunctioinByVal from tmp val" << endl;
+    fooPassToFunctioinByVal(Model("tag1"));
+}
+
+void fooPassToFunctioinByPtr(Model * model) {
 
 }
 
@@ -23,7 +42,16 @@ void testPassToFunctioinByPtr() {
 
 }
 
+void fooPassToFunctioinByRef(Model & model) {
+
+}
+
+void fooPassToFunctioinByConstRef(Model const & model) {
+
+}
+
 void testPassToFunctioinByRef() {
+
 
 }
 
